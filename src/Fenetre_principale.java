03e4d0871cfcccd2efbbbb1e9b2048331569acba
NicoFRI <nicofritsch@hotmail.fr>
2016-03-14 15:48:08 +0100

    base_vue

--- src/Fenetre_principale.java
+++ src/Fenetre_principale.java
@@ -1,4 +1,23 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public interface Fenetre_principale {
+import javax.swing.JFrame;
 
+public class Fenetre_principale extends JFrame implements ActionListener  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8498480202498074223L;
+
+
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
